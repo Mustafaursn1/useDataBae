52679e8e8d97d6c9e019c287372502a6118c47fd
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2024-11-24 21:38:45 +0100

    add delete from table

--- src/main/java/Baglanti.java
+++ src/main/java/Baglanti.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Baglanti {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Baglanti {
+
+    private String kullanici_adi = "root";
+    private String paralo = "";
+    private String db_ismi = "demo";
+    private String host = "localhost";
+    private int port = 8888;
+    private Connection con = null;
+    private String demoUrl="http://localhost:8888/phpMyAdmin/db_structure.php?server=1&db=demo";
+
+    public Baglanti() {
+        String url = "jdbc:mysql://" + host + ":" + paralo + "/" + db_ismi;
+        try {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                con = DriverManager.getConnection(url, kullanici_adi, paralo);
+                System.out.println("Baglanti basarili.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Baglanti baglanti=new Baglanti();
+
+
+    }
 }
--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -1,38 +1,77 @@
 import java.sql.*;
 
+import static java.sql.DriverManager.getConnection;
+
 public class Connect {
     private String userName = "root";
     private String password = "";
     private String nameDataBase = "demo";
     private String host = "localhost";
-    private int port = 3306;
-    private Connection con = null;
-    private Statement statement = null;
+    private int port = 8888;
+    private Connection con;
+    private Statement statement;
 
+    public void deleteEmployee(){
+        try {
+            statement=con.createStatement();
+            String query="Delete from employees where id>3";
+           int i= statement.executeUpdate(query);
+            System.out.println(i);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateEmployee() {
+
+        try {
+            statement= con.createStatement();
+            String query="Update employees Set email ='tk.12@hotmail.com' where id=1";
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addEmployee() {
+        try {
+            statement = con.createStatement();
+            String fristName = "Semih";
+            String lastName = "Akkoyun";
+            String email = "smh.2020@gmail.com";
+            String query = "insert into employees (firsname,lastname,email) VALUES (" + "'" + fristName + "'," + lastName + "'," + email + ",)";
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public void getEmployee() {
         String query = "Select * from employees ";
         try {
-            statement =con.createStatement();
+            statement = con.createStatement();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
         try {
-           ResultSet rs= statement.executeQuery(query);
-           while (rs.next()){
-               int id=rs.getInt("id");
-               String name=rs.getString("firstname");
-               String lastName=rs.getString("lastname");
-               String email=rs.getString("email");
-               System.out.println("Id: "+id+"name: "+name+"lastname: "+lastName+"email: "+email);
-           }
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("firstname");
+                String lastName = rs.getString("lastname");
+                String email = rs.getString("email");
+                System.out.println("Id: " + id + "name: " + name + "lastname: " + lastName + "email: " + email);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public Connect() {
+
         String url = "jdbc:mysql://" + host + ":" + port + "/" + nameDataBase;
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -40,7 +79,7 @@ public class Connect {
             System.out.println("Driver is not found ");
         }
         try {
-            con = DriverManager.getConnection(url, userName, password);
+            con = getConnection(url, userName, password);
             System.out.println("Connection with Database is successful");
         } catch (SQLException e) {
             //System.out.println("Connection with Database is unsuccessful");
@@ -52,6 +91,12 @@ public class Connect {
     public static void main(String[] args) {
         Connect connect = new Connect();
         connect.getEmployee();
+
+        System.out.println("before.....");
+        connect.addEmployee();
+
+        connect.updateEmployee();
+        connect.deleteEmployee();
 
 
     }
